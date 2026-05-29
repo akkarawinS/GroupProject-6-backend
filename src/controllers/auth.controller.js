5e4836c1ea7138c311a43bfe48d751a08f742869
AkkarawinS <akkarawinRX@gmail.com>
2026-05-29 12:38:24 +0700

    login route work on my machine

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import { User } from '../models/user.model.js';
+import { comparePassword } from '../middlewares/comparePassword.js'
+import jwt from 'jsonwebtoken';
 
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
     try {
         const { username, email, password } = req.body || {};
         const existUser = await User.findOne({ email });
@@ -23,3 +25,50 @@ export const register = async (req, res) => {
 
     }
 };
+
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).send({ message: "Username or password are required" })
+        };
+
+        const findUser = await User.findOne({ email }).select('+password');
+        const isMatch = await comparePassword(password, findUser.password);
+
+        if (!findUser || !isMatch) {
+            return res.status(400).send({ message: "อีเมลหรือรหัสผ่านไม่ถูกต้อง" })
+        };
+
+        const token = jwt.sign({ userId: findUser._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        const isProd = process.env.NODE_ENV === 'production'
+
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: isProd, // Only send over HTTPS in production
+            sameSite: isProd ? "none" : "lax",
+            path: "/",
+            maxAge: 60 * 60 * 1000, // 1HR its age of cookie
+        })
+
+
+        res.status(200).json({
+            success: true,
+            message: 'เข้าสู่ระบบสำเร็จ!',
+            user: {
+                _id: findUser._id,
+                username: findUser.username,
+                email: findUser.email,
+                role: findUser.role,
+            },
+        });
+
+
+    } catch (err) {
+        next(err)
+    }
+}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
-import { register } from '../controllers/auth.controller.js'
+import { login, register } from '../controllers/auth.controller.js'
 
 export const router = Router()
 
 router.post('/register', register);
+
+router.post('/login' , login);
