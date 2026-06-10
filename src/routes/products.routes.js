fb4e61f867580fcb6e8ff2dd41292a3382236568
Kasidate444 <kasidatesaeeaw@gmail.com>
2026-06-10 14:28:05 +0700

    add admin able to delete product cart at shop

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,23 +1,53 @@
-import { Router } from 'express';
-import { authUser } from '../middlewares/authen.middleware.js';
-import { isArtistOrAdmin } from '../middlewares/roleCheck.middleware.js';
-import { createAlbumProduct, createMerchProduct, createSingleProduct, getAllProductInfo, getProductById } from '../controllers/product.controller.js';
-import { uploadProductFiles } from '../middlewares/uploadFiles.middleware.js';
+import { Router } from "express";
+import { authUser } from "../middlewares/authen.middleware.js";
+import {
+  isArtistOrAdmin,
+  isAdmin,
+} from "../middlewares/roleCheck.middleware.js";
+import {
+  createAlbumProduct,
+  createMerchProduct,
+  createSingleProduct,
+  getAllProductInfo,
+  getProductById,
+  deleteProduct,
+} from "../controllers/product.controller.js";
+import { uploadProductFiles } from "../middlewares/uploadFiles.middleware.js";
 
 export const router = Router();
 
-router.get('/products', getAllProductInfo);
+router.get("/products", getAllProductInfo);
 
-router.get('/products/:productId', getProductById);
+router.get("/products/:productId", getProductById);
 
-router.post('/products/single',authUser,isArtistOrAdmin,uploadProductFiles.fields([{ name: 'audio', maxCount: 1 },{ name: 'cover', maxCount: 1 },]),createSingleProduct,);
+router.delete("/products/:productId", authUser, isAdmin, deleteProduct);
 
+router.post(
+  "/products/single",
+  authUser,
+  isArtistOrAdmin,
+  uploadProductFiles.fields([
+    { name: "audio", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  createSingleProduct,
+);
 
-router.post('/products/album',authUser,isArtistOrAdmin,uploadProductFiles.fields([
-      { name: 'audio', maxCount: 20 },
-      { name: 'cover', maxCount: 1 },
-    ]),createAlbumProduct);
+router.post(
+  "/products/album",
+  authUser,
+  isArtistOrAdmin,
+  uploadProductFiles.fields([
+    { name: "audio", maxCount: 20 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  createAlbumProduct,
+);
 
-router.post('/products/merch',authUser,isArtistOrAdmin,uploadProductFiles.fields([
-      { name: 'cover', maxCount: 1 },
-    ]),createMerchProduct);
+router.post(
+  "/products/merch",
+  authUser,
+  isArtistOrAdmin,
+  uploadProductFiles.fields([{ name: "cover", maxCount: 1 }]),
+  createMerchProduct,
+);
